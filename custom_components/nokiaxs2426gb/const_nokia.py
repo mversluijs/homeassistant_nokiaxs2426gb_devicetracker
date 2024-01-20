27b6ef484d486a27020401302a7111028ce5f985
Martijn Versluijs <martijn.versluijs@gmail.com>
2024-01-20 09:25:33 +0100

    fix for new device software

--- custom_components/nokiaxs2426gb/const_nokia.py
+++ custom_components/nokiaxs2426gb/const_nokia.py
@@ -4,4 +4,4 @@ DEFAULT_USER = "admin"
 LOGIN_NONCE_API = '/login_web_app.cgi?nonce'
 LOGIN_API = '/login_web_app.cgi'
 LOGGED_IN_API = '/check_expire_web_app.cgi'
-DEVICE_INFO_API = '/device_status_web_app.cgi'
+DEVICE_INFO_API = '/device_status_web_app.cgi?getroot'
